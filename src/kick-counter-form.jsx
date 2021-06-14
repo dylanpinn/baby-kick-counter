9171c3fc653f68c36e1cf1a86057c4778bc213a0
Dylan Pinn <me@dylanpinn.com>
2021-06-14 17:30:38 +1000

    Capture when kicks are happening

--- src/kick-counter-form.jsx
+++ src/kick-counter-form.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 export default function KickCounterForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const dateTimeNow = new Date();
+    console.log("kick recorded at", dateTimeNow.toLocaleString());
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <button type="submit">Kick</button>
     </form>
   );
