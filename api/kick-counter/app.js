84b8260f21f2e7d094f93b9859eddd6fb1d5a590
Dylan Pinn <me@dylanpinn.com>
2021-06-14 18:54:41 +1000

    Web and API should work

--- api/kick-counter/app.js
+++ api/kick-counter/app.js
@@ -1,33 +1,39 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+const tableName = process.env.KICK_COUNTER_TABLE;
 
 /**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
+ * Record the time of kicks in DynamoDB table.
  */
 exports.lambdaHandler = async (event, context) => {
-  try {
-    // const ret = await axios(url);
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "hello world",
-        // location: ret.data.trim()
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return err;
+  if (event.httpMethod !== "POST") {
+    throw new Error(
+      `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
+    );
   }
+  // All log statements are written to CloudWatch
+  console.info("received:", event);
 
+  // Get time from the body.
+  const body = JSON.parse(event.body);
+  const kickAt = body.kickAt;
+
+  // Creates a new item.
+  const params = {
+    TableName: tableName,
+    Item: { id: kickAt },
+  };
+
+  const result = await docClient.put(params).promise();
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
   return response;
 };
--- web/src/kick-counter-form.jsx
+++ web/src/kick-counter-form.jsx
@@ -5,6 +5,25 @@ export default function KickCounterForm() {
     e.preventDefault();
     const dateTimeNow = new Date();
     console.log("kick recorded at", dateTimeNow.toLocaleString());
+
+    sendData(Number(dateTimeNow)).then((data) => {
+      console.log(data);
+    });
+  }
+
+  async function sendData(kickAt) {
+    try {
+      const url =
+        "https://pg6zk1qbhl.execute-api.ap-southeast-2.amazonaws.com/Prod/kick";
+      const data = { kickAt };
+      const response = await fetch(url, {
+        method: "POST",
+        body: JOSN.stringify(data),
+      });
+      return response.json();
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   return (
