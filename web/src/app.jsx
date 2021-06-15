e4161dd5c054bf3ad6adfe91dcb855b65d3a192e
Dylan Pinn <me@dylanpinn.com>
2021-06-15 13:10:28 +1000

    Add margin top at top of page

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -4,7 +4,7 @@ import KickCounterForm from "./kick-counter-form";
 export default function App() {
   return (
     <React.Fragment>
-      <h1 className="font-sans text-4xl text-center">
+      <h1 className="font-sans text-4xl text-center mt-4">
         Let's Count Baby Kicks...
       </h1>
       <KickCounterForm />
